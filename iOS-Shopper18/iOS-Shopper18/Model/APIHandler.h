ca5716f39fea94604cf7a6e944223c25d0e29248
Alvin <ling94alvin@gmail.com>
2019-05-02 17:23:12 -0500

    apihandler started

--- iOS-Shopper18/iOS-Shopper18/Model/APIHandler.h
+++ iOS-Shopper18/iOS-Shopper18/Model/APIHandler.h
@@ -12,6 +12,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface APIHandler : NSObject
 
+@property (strong, nonnull) NSString *apiKey;
+
++ (instancetype)shared;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- iOS-Shopper18/iOS-Shopper18/Model/APIHandler.m
+++ iOS-Shopper18/iOS-Shopper18/Model/APIHandler.m
@@ -10,4 +10,30 @@
 
 @implementation APIHandler
 
++ (instancetype)shared {
+    static dispatch_once_t pred = 0;
+    static id _shared = nil;
+    dispatch_once(&pred, ^{
+        _shared = [[self alloc] init];
+    });
+    return _shared;
+}
+
+- (NSString *)constructURL:(NSString *)base params:(NSDictionary *)params {
+    NSString *result = [NSMutableString.alloc initWithString:base];
+    for (NSString *key in params) {
+        
+    }
+    return @"";
+}
+
+- (void)registerUser:(NSDictionary *)userInfo completion:(void(^)(NSError * _Nullable))completion {
+    if (!self.apiKey) {
+        completion(nil);
+        return;
+    }
+    
+    
+}
+
 @end
