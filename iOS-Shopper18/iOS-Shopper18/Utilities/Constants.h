2376787644753de5f897e2598b9f0e316eb5be0f
Alvin <ling94alvin@gmail.com>
2019-05-02 17:30:59 -0500

    add prefix header and constants

---
+++ iOS-Shopper18/iOS-Shopper18/Utilities/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/2/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+#define kRegistrationURL @"http://rjtmobile.com/aamir/e-commerce/ios-app/shop_reg.php?"
+
+#endif /* Constants_h */
