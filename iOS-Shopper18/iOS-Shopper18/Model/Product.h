9eeff9555eb4164aa95a52416d36e5c9392fd3db
Alvin <ling94alvin@gmail.com>
2019-05-03 00:41:21 -0500

    add product models

---
+++ iOS-Shopper18/iOS-Shopper18/Model/Product.h
@@ -0,0 +1,25 @@
+//
+//  Product.h
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/3/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Product : NSObject
+
+@property (strong, nonatomic, readonly) NSString
+    *pid,
+    *name,
+    *quantity,
+    *prize,
+    *desc,
+    *imageURL;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS-Shopper18/iOS-Shopper18/Model/Product.m
@@ -0,0 +1,39 @@
+//
+//  Product.m
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/3/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import "Product.h"
+
+@interface Product ()
+
+@property (readwrite) NSString
+    *pid,
+    *name,
+    *quantity,
+    *prize,
+    *desc,
+    *imageURL;
+
+@end
+
+@implementation Product
+
++ (instancetype)initWithInfo:(NSDictionary *)info {
+    Product *product = [self init];
+    if (product) {
+        product.pid = info[@"id"];
+        product.name = info[@"pname"];
+        product.quantity = info[@"quantity"];
+        product.prize = info[@"prize"];
+        product.desc = info[@"discription"];
+        product.imageURL = info[@"image"];
+    }
+    return product;
+}
+
+@end
+
