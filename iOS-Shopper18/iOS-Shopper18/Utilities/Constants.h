44bda6944d9cac08d6888317563c762648231f61
Alvin <ling94alvin@gmail.com>
2019-05-02 20:37:39 -0500

    macros used for apiahandler added

--- iOS-Shopper18/iOS-Shopper18/Utilities/Constants.h
+++ iOS-Shopper18/iOS-Shopper18/Utilities/Constants.h
@@ -9,6 +9,25 @@
 #ifndef Constants_h
 #define Constants_h
 
-#define kRegistrationURL @"http://rjtmobile.com/aamir/e-commerce/ios-app/shop_reg.php?"
+#define kAPIEcomBase            @"http://rjtmobile.com/aamir/e-commerce/ios-app/"
+
+#define kAPIEndPointRegister    @"shop_reg.php?"
+#define kAPIEndPointLogin       @"shop_login.php?"
+#define kAPIEndPointProfile     @"edit_profile.php?"
+#define kAPIEndPointPassw       @"shop_reset_pass.php?"
+#define kAPIEndPointForgot      @"forgot_pass_email.php?"
+#define kAPIEndPointMakeOrder   @"orders.php?"
+#define kAPIEndPointOrderHist   @"order_history.php?"
+#define kAPIEndPointShipTrack   @"shipment_track.php?"
+#define kAPIEndPointCoupon      @"coupon.php?"
+#define kAPIEndPointProfPic     @"profile_pic_upload.php?"
+#define kAPIEndPointTopSellers  @"shop_top_sellers.php?"
+
+
+#define kAPICartBase            @"http://rjtmobile.com/ansari/shopingcart/ios-ssl/"
+
+#define kAPIEndPointCategory    @"cust_category.php?"
+#define kAPIEndPointSubCategory @"cust_sub_category.php?"
+#define kAPIEndPointProductList @"product_details.php?"
 
 #endif /* Constants_h */
