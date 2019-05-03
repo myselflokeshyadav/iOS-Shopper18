3366d93c97cb90ce8d0e9c167352478395f9f362
Alvin <ling94alvin@gmail.com>
2019-05-03 01:28:51 -0500

    add model parser

---
+++ iOS-Shopper18/iOS-Shopper18/Model/APIParser.h
@@ -0,0 +1,24 @@
+//
+//  APIParser.h
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/3/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@class Product;
+@class Category;
+@class Order;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface APIParser : NSObject
+
++ (nullable NSArray<Category *> *)categoriesFrom:(id)jsonObject;
++ (nullable NSArray<Product *> *)productsFrom:(id)jsonObject;
++ (nullable NSArray<Order *> *)ordersFrom:(id)jsonObject;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS-Shopper18/iOS-Shopper18/Model/APIParser.m
@@ -0,0 +1,44 @@
+//
+//  APIParser.m
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/3/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import "APIParser.h"
+#import "Category.h"
+#import "Product.h"
+#import "Order.h"
+
+@implementation APIParser
+
++ (nullable NSArray<Category *> *)categoriesFrom:(id)jsonObject {
+    NSDictionary *dict = jsonObject;
+    
+    NSArray *categories = (dict[@"category"])? dict[@"category"] : dict[@"subcategory"];
+    NSMutableArray *list = NSMutableArray.new;
+    for (NSDictionary *info in categories)
+        [list addObject:[Category initWithInfo:info]];
+    return list;
+}
+
++ (nullable NSArray<Product *> *)productsFrom:(id)jsonObject {
+    NSArray *products = ((NSDictionary *)jsonObject)[@"products"];
+    NSMutableArray *list = NSMutableArray.new;
+    for (NSDictionary *info in products)
+        [list addObject:[Product initWithInfo:info]];
+    return list;
+}
+
+
++ (nullable NSArray<Order *> *)ordersFrom:(id)jsonObject {
+    NSDictionary *dict = jsonObject;
+    NSArray *orders = (dict[@"Order confirmed"])? dict[@"Order confirmed"] : dict[@"Order history"];
+    NSMutableArray *list = NSMutableArray.new;
+    for (NSDictionary *info in orders)
+        [list addObject:[Order initWithInfo:info]];
+    return list;
+}
+
+@end
