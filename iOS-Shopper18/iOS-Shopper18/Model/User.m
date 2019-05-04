6bfc3efb73aa6367ba9dce7719281922045efa60
Alvin <ling94alvin@gmail.com>
2019-05-04 17:20:47 -0500

    add user model

---
+++ iOS-Shopper18/iOS-Shopper18/Model/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/4/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
