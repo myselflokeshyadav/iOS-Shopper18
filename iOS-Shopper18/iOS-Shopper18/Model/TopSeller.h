dcd718f4e947e870a61f7c104390ee76c1b49b70
Alvin <ling94alvin@gmail.com>
2019-05-04 20:23:53 -0500

    prepare files for rest of sprint

---
+++ iOS-Shopper18/iOS-Shopper18/Model/TopSeller.h
@@ -0,0 +1,17 @@
+//
+//  TopSeller.h
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/4/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TopSeller : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
