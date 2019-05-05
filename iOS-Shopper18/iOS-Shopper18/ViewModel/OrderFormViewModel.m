00743125fc4cc6f0cfeb841f4116470eac33dd0f
Alvin <ling94alvin@gmail.com>
2019-05-04 21:00:20 -0500

    view models for order form

---
+++ iOS-Shopper18/iOS-Shopper18/ViewModel/OrderFormViewModel.m
@@ -0,0 +1,13 @@
+z//
+//  OrderFormViewModel.m
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/4/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import "OrderFormViewModel.h"
+
+@implementation OrderFormViewModel
+
+@end
