4a952f958c21e672ceceb8c89876b0430a082956
Alvin <ling94alvin@gmail.com>
2019-05-05 13:52:52 -0500

    product detail classes

---
+++ iOS-Shopper18/iOS-Shopper18/Controller/ProductDetailViewController.h
@@ -0,0 +1,26 @@
+//
+//  ProductDetailViewController.h
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/5/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class ProductDetailViewModel;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProductDetailViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *productImage;
+@property (weak, nonatomic) IBOutlet UILabel *productName;
+@property (weak, nonatomic) IBOutlet UILabel *productPrize;
+@property (weak, nonatomic) IBOutlet UILabel *productQuantity;
+@property (weak, nonatomic) IBOutlet UILabel *productDescribtion;
+
+@property (strong, nonatomic) ProductDetailViewModel *vm;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS-Shopper18/iOS-Shopper18/ViewModel/ProductDetailViewModel.h
@@ -0,0 +1,26 @@
+//
+//  ProductDetailViewModel.h
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/5/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//#import <ReactiveCocoa/ReactiveCocoa.h>
+//#import "ProductStore.h"
+#import "Product.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProductDetailViewModel : NSObject
+
++ (instancetype)initWithProduct:(Product *)product;
+
+- (NSString *)productName;
+- (NSString *)showProductName;
+- (NSString *)showDescription;
+
+@end
+
+NS_ASSUME_NONNULL_END
