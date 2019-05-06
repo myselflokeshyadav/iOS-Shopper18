be0285c9c9a814536f04d4f34463c4df338d7119
Alvin <ling94alvin@gmail.com>
2019-05-06 09:08:58 -0500

    add cart singleton

---
+++ iOS-Shopper18/iOS-Shopper18/Model/Cart.h
@@ -0,0 +1,17 @@
+//
+//  Cart.h
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/6/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Cart : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS-Shopper18/iOS-Shopper18/Model/Cart.m
@@ -0,0 +1,13 @@
+//
+//  Cart.m
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/6/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import "Cart.h"
+
+@implementation Cart
+
+@end
