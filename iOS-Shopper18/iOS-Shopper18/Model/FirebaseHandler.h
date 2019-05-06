2160036d23edf01a10c48722cf3af6cf98f03a15
Alvin <ling94alvin@gmail.com>
2019-05-05 23:26:21 -0500

    add firebase handlers

---
+++ iOS-Shopper18/iOS-Shopper18/Model/FirebaseHandler.h
@@ -0,0 +1,17 @@
+//
+//  FirebaseHandler.h
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/5/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FirebaseHandler : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS-Shopper18/iOS-Shopper18/Model/FirebaseHandler.m
@@ -0,0 +1,13 @@
+//
+//  FirebaseHandler.m
+//  iOS-Shopper18
+//
+//  Created by Alvin Ling on 5/5/19.
+//  Copyright © 2019 iOSPlayground. All rights reserved.
+//
+
+#import "FirebaseHandler.h"
+
+@implementation FirebaseHandler
+
+@end
