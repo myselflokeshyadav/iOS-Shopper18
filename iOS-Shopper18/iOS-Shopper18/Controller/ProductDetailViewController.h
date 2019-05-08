80d8cd6fa9d6abf0fcffc0efd26da51216cefcb9
Noh Tewolde <noh.tewolde@kysu.edu>
2019-05-08 15:26:43 -0500

    <UIDocumentInteractionControllerDelegate>  added to product detail

--- iOS-Shopper18/iOS-Shopper18/Controller/ProductDetailViewController.h
+++ iOS-Shopper18/iOS-Shopper18/Controller/ProductDetailViewController.h
@@ -12,7 +12,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ProductDetailViewController : UIViewController
+@interface ProductDetailViewController : UIViewController <UIDocumentInteractionControllerDelegate> 
 
 @property (weak, nonatomic) IBOutlet UIImageView *productImage;
 @property (weak, nonatomic) IBOutlet UILabel *productName;
